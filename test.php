603087c5ac463477052c261b36c24ed13908f0b4
JHUGHES9 <JHUGHES9@stu.chi.ac.uk>
2022-03-17 19:26:36 +0000

    added local test.php to git so team can see what is running

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+include 'functions.php';
+
+
+
+
+get_image_url(1);
+echo "hi";
+//updateEvent("a8ddae56-88ea-5001-555f-746b4b49e938", "get_in_a_building", "19", "good_description", "12", "black.png");
+?>
