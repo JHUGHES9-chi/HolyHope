2a55d1eebe72b111a51f9d14bdcc5336e31fed77
faizannaveed <57486515+faizannaveed@users.noreply.github.com>
2022-03-17 18:49:28 +0000

    product crud

---
+++ deleteproduct.php
@@ -0,0 +1,55 @@
+<!--The delete page will be used to delete records from the events table. Before a user can delete a record they will need to confirm it, this will prevent accidental deletion. -->
+<?php
+include 'functions.php';
+session_start();
+if(!isset($_SESSION['username']))
+{
+  header('location: index.php');
+}
+
+$pdo = pdo_connect_mysql();
+$msg = '';
+// Check that the event ID exists
+if (isset($_GET['id'])) {
+    // Select the record that is going to be deleted
+    $stmt = $pdo->prepare('SELECT * FROM products WHERE id = ?');
+    $stmt->execute([$_GET['id']]);
+    $product = $stmt->fetch(PDO::FETCH_ASSOC);
+    if (!$product) {
+        exit('Product doesn\'t exist with that ID!');
+    }
+    // Make sure the user confirms before deletion
+    if (isset($_GET['confirm'])) {
+        if ($_GET['confirm'] == 'yes') {
+            // User clicked the "Yes" button, delete record
+            $stmt = $pdo->prepare('DELETE FROM products WHERE id = ?');
+            $stmt->execute([$_GET['id']]);
+            $msg = 'You have deleted the event!';
+            header('Location: readproducts.php');
+        } else {
+            // User clicked the "No" button, redirect them back to the read page
+            header('Location: readproducts.php');
+            exit;
+        }
+    }
+} else {
+    exit('No ID specified!');
+}
+?>
+
+<!--To delete a record the code will check if the GET request variable "id" exists, if it does then check if the record exists in the events table and confirm the user if they would like to delete the event or not, a simple GET request will determine which button the user clicked (Yes or No). -->
+    <?php include 'header.php'; ?>
+        <div class="content delete">
+            <h2>Delete event #<?=$product['id']?></h2>
+            <?php if ($msg): ?>
+            <p><?=$msg?></p>
+            <?php else: ?>
+            <p>Are you sure you want to delete event #<?=$product['id']?>?</p>
+            <div class="yesno">
+                <a href="deleteproduct.php?id=<?=$product['id']?>&confirm=yes">Yes</a>
+                <a href="deleteproduct.php?id=<?=$product['id']?>&confirm=no">No</a>
+            </div>
+            <?php endif; ?>
+        </div>
+        
+<?php include 'footer.php'; ?>
---
+++ editproduct.php
@@ -0,0 +1,67 @@
+<?php
+include 'functions.php';
+session_start();
+if(!isset($_SESSION['username']))
+{
+  header('location: index.php');
+}
+
+$pdo = pdo_connect_mysql();
+$msg = '';
+// Check if the events id exists, for example update.php?id=1 will get the events with the id of 1
+if (isset($_GET['id'])) {
+    if (!empty($_POST)) {
+        // This part is similar to the create.php, but instead updates a record and not insert
+        $name = isset($_POST['name']) ? $_POST['name'] : '';
+        $handleId = isset($_POST['handleid']) ? $_POST['handleid'] : '';
+        $productimageurl = isset($_POST['productimageurl']) ? $_POST['productimageurl'] : '';
+        $collection = isset($_POST['collection']) ? $_POST['collection'] : '';
+        $price = isset($_POST['price']) ? $_POST['price'] : '';
+        $inventory = isset($_POST['inventory']) ? $_POST['inventory'] : '';
+        $description = isset($_POST['description']) ? $_POST['description'] : '';
+        // Update the record
+        $stmt = $pdo->prepare('UPDATE products SET name = ?, name = ?, handleid = ?, productimageurl = ?, collection = ?, price = ?, inventory = ?, description = ? WHERE id = ?');
+        $stmt->execute([$id, $name, $handleId, $productimageurl, $collection , $price, $inventory, $description, $_GET['id']]);
+        $msg = 'Updated Successfully!';
+        header('Location: readproducts.php');
+    }
+    // Get the events from the Events table
+    $stmt = $pdo->prepare('SELECT * FROM products WHERE id = ?');
+    $stmt->execute([$_GET['id']]);
+    $product = $stmt->fetch(PDO::FETCH_ASSOC);
+    if (!$product) {
+        exit('products doesn\'t exist with that ID!');
+    }
+} else {
+    exit('No ID specified!');
+}
+?>
+
+<?php include 'header.php'; ?>
+        <div class="contact-section">
+            <h2>Product #<?=$product['id']?></h2> 
+            <form action="editproduct.php?id=<?=$product['id']?>" method="post">
+                    <label for="productid">Product id</label>
+                    <input type="number" name="id" id="id"class="contact-form-text" placeholder="Product id" value="<?=$product['id']?>" required>
+                    <label for="name">Product Name</label>
+                    <input type="text" name="name" id="name" class="contact-form-text" placeholder="Product Name" value="<?=$product['name']?>" required>
+                    <label for="name">Product Handle Id</label>
+                    <input type="text" name="name" id="name" class="contact-form-text" placeholder="Product Handle Id" value="<?=$product['handleid']?>" required>
+                    <label for="name">Image Url</label>
+                    <input type="text" name="name" id="name" class="contact-form-text" placeholder="Image url" value="<?=$product['productimageurl']?>" required>
+                    <label for="name">Collection</label>
+                    <input type="text" name="name" id="name" class="contact-form-text" placeholder="Collection" value="<?=$product['collection']?>" required>
+                    <label for="name">Product Price</label>
+                    <input type="decimals" name="price" id="price" class="contact-form-text" placeholder="Product price" value="&pound; <?=$product['price']?>" required>
+                    <label for="productid">Inventory</label>
+                    <input type="number" name="id" id="id"class="contact-form-text" placeholder="Inventory" value="<?=$product['inventory']?>" required>
+                    <label for="description">Description</label>
+                    <input type="text" name="description" placeholder="enter some description" value="<?=$product['description']?>" id="description">
+                    <input type="submit" name="submit" class="contact-button" value="update">
+                    <input type="button" class="contact-button" value="Go back!" onclick="history.go(-1)">
+            </form>
+            <?php if ($msg): ?>
+            <?php endif; ?>
+        </div>
+
+    <?php include 'footer.php'; ?>
