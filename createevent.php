581cd4daf48f422ff9aefb5465a91d3099e4575b
faizannaveed <57486515+faizannaveed@users.noreply.github.com>
2022-03-18 23:28:51 +0000

    added new fields to the form

--- createevent.php
+++ createevent.php
@@ -7,25 +7,28 @@ if(!isset($_SESSION['username']))
 }
 
 
-$name = $price = $location = $event_date = $description = "";
+$name = $price = $image = $stock = $description = $product_page = "";
 $msg = '';
 // Check if POST data is not empty
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $pdo = pdo_connect_mysql();
 
       $pdo = pdo_connect_mysql();
-        $stmt = $pdo->prepare("INSERT INTO events (name, price , location, event_date, description) VALUES (:name, :price, :location, :event_date, :description)");
+        $stmt = $pdo->prepare("INSERT INTO events (name, price , image, stock, description, product_page) VALUES (:name, :price, :image, :stock, :description, :product_page)");
         $stmt->bindParam(':name', $name);
         $stmt->bindParam(':price', $price);
-        $stmt->bindParam(':location', $location);
-        $stmt->bindParam(':event_date', $event_date);
+        $stmt->bindParam(':image', $image);
+        $stmt->bindParam(':stock', $stock);
         $stmt->bindParam(':description', $description);
+        $stmt->bindParam(':product_page', $product_page);
+
 
         $name = clean($_POST["name"]);
         $price = clean($_POST["price"]);
-        $location = clean($_POST["location"]);
-        $event_date = clean($_POST["event_date"]);
+        $image = clean($_POST["image"]);
+        $stock = clean($_POST["stock"]);
         $description = clean($_POST["description"]);
+        $product_page = clean($_POST["product_page"]);
         $stmt->execute();
 
     echo "<div class='event-center'><h2>New Event created</h2>";
@@ -33,11 +36,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "<br>";
     echo "<b>Event Price :</b> ". $price;
     echo "<br>";
-    echo "<b>Event Location :</b> " . $location;
+    echo "<b>Event Image :</b> " . $image;
     echo "<br>";
-    echo "<b>Event Date :</b>" . $event_date;
+    echo "<b>Event Stock :</b>" . $stock;
     echo "<br>";
     echo "<b>Event Description :</b>" . $description;
+    echo "<br>";
+    echo "<b>Product Page :</b>" . $product_page;
     echo "<br>";
     echo "<h2>New event is added to the database</h2></div> ";
 
@@ -57,11 +62,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <input type="text" name="name" id="name" class="contact-form-text" placeholder="Event Name" required>
                 <label for="name">Event price</label>
                 <input type="decimals" name="price" id="price" class="contact-form-text" placeholder="Event Price" required>
-                <label for="name">Event Date</label>
-                <input type="date" name="event_date" id="event_date" class="contact-form-text" value="2021-03-22" required>
-                <label for="name">Event Location</label>
-                <input type="text" name="location" id="location" class="contact-form-text" placeholder="Event location" required>
-                <textarea name="description" class="contact-form-text" placeholder="event description"></textarea>
+                <label for="name">Image Url</label>
+                <input type="text" name="image" id="image" class="contact-form-text" placeholder="Image url" required>
+                <label for="name">Stock</label>
+                <input type="number" name="stock" id="stock" class="contact-form-text" placeholder="Stock" required>
+                <label for="name">Wix Page</label>
+                <input type="text" name="product_page" id="product_page" class="contact-form-text" placeholder="product page url" required>
+                <label for="description">Description : </label>
+                <textarea class="contact-form-text" name="description" id="description" rows="5" cols="100">Description</textarea>
                 <input type="submit" name="submit" class="contact-button" value="Create">
                 <button onclick="goBack()">Go Back</button>
 
