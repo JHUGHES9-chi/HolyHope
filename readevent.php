9d460cb171369c22cac246613dc9bb40c15661b0
faizannaveed <57486515+faizannaveed@users.noreply.github.com>
2022-03-18 22:44:33 +0000

    read events update

--- readevent.php
+++ readevent.php
@@ -7,14 +7,14 @@ if(!isset($_SESSION['username']))
 }
 $pdo = pdo_connect_mysql();
 // The amounts of products to show on each page
-$num_products_on_each_page = 4;
+$num_products_on_each_page = 5;
 // The current page, in the URL this will appear as readevent.php?page=products&p=1, readevent.php?page=products&p=2, etc...
 $current_page = isset($_GET['p']) && is_numeric($_GET['p']) ? (int)$_GET['p'] : 1;
 // Select products ordered by the date added
-$stmt = $pdo->prepare('SELECT * FROM events');
+$stmt = $pdo->prepare('SELECT * FROM events LIMIT ?,?');
 // bindValue will allow us to use integer in the SQL statement, we need to use for LIMIT
-//$stmt->bindValue(1, ($current_page - 1) * $num_products_on_each_page, PDO::PARAM_INT);
-//$stmt->bindValue(2, $num_products_on_each_page, PDO::PARAM_INT);
+$stmt->bindValue(1, ($current_page - 1) * $num_products_on_each_page, PDO::PARAM_INT);
+$stmt->bindValue(2, $num_products_on_each_page, PDO::PARAM_INT);
 $stmt->execute();
 // Fetch the products from the database and return the result as an Array
 $events = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -37,13 +37,6 @@ $total_products = $pdo->query('SELECT * FROM events')->rowCount();
           <h1><?= $event['name']?></h1>
         </div>
         <div class="card-body">
-          <p>
-          <?= $event['description']?>
-          </p>
-          <br/>
-          <h5>
-          <?= $event['event_date']?>
-        </h5>
           <a href="viewevent.php?id=<?=$event['id']?>" class="btn">View Event Information</a>
         </div>
       </div>
