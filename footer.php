80745870e23303f04d0fd57f160839d8b6cd46ee
Benjamin woods-ives <91830838+Benji123456789@users.noreply.github.com>
2022-03-21 21:51:29 +0000

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="contact-content text-center">
       <a href="readevent.php">HolyHope</a>
-      <p> HolyHope is an art business which offers range of art products</p>
+      <p> HolyHope is an art business which offers a range of art products and events.</p>
   </footer>
 </body>
